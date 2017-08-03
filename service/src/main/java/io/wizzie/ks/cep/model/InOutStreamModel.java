c07bf40a050b92f4945896badfddb8f854ed2a69
Juan Jesus Chorro Bergillo <jjchorrobe@gmail.com>
2017-08-03 10:21:17 +0200

    Implement POJO

--- service/src/main/java/io/wizzie/ks/cep/model/InOutStreamModel.java
+++ service/src/main/java/io/wizzie/ks/cep/model/InOutStreamModel.java
@@ -1,9 +1,43 @@
 package io.wizzie.ks.cep.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class InOutStreamModel {
     List<SourceModel> sources;
     List<SinkModel> sinks;
     List<StreamModel> streams;
+
+    public InOutStreamModel(@JsonProperty("sources") List<SourceModel> sources,
+                            @JsonProperty("sinks") List<SinkModel> sinks,
+                            @JsonProperty("streams") List<StreamModel> streams) {
+        this.sources = sources;
+        this.sinks = sinks;
+        this.streams = streams;
+    }
+    @JsonProperty
+    public List<SourceModel> getSources() {
+        return sources;
+    }
+    @JsonProperty
+    public void setSources(List<SourceModel> sources) {
+        this.sources = sources;
+    }
+    @JsonProperty
+    public List<SinkModel> getSinks() {
+        return sinks;
+    }
+    @JsonProperty
+    public void setSinks(List<SinkModel> sinks) {
+        this.sinks = sinks;
+    }
+    @JsonProperty
+    public List<StreamModel> getStreams() {
+        return streams;
+    }
+    @JsonProperty
+    public void setStreams(List<StreamModel> streams) {
+        this.streams = streams;
+    }
 }
