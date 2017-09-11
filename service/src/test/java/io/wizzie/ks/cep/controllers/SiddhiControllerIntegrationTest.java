892e3277754a69686e3153699da6729333a37f85
Pablo Casares <lito92vforce@gmail.com>
2017-09-11 11:26:34 +0200

    WIZZIE-48 Fix filter out bug for empty messages.

--- service/src/test/java/io/wizzie/ks/cep/controllers/SiddhiControllerIntegrationTest.java
+++ service/src/test/java/io/wizzie/ks/cep/controllers/SiddhiControllerIntegrationTest.java
@@ -44,6 +44,7 @@ public class SiddhiControllerIntegrationTest {
         CLUSTER.createTopic("input6", 1, REPLICATION_FACTOR);
         CLUSTER.createTopic("input7", 1, REPLICATION_FACTOR);
         CLUSTER.createTopic("input8", 1, REPLICATION_FACTOR);
+        CLUSTER.createTopic("input9", 1, REPLICATION_FACTOR);
 
 
 
@@ -56,6 +57,7 @@ public class SiddhiControllerIntegrationTest {
         CLUSTER.createTopic("output66", 1, REPLICATION_FACTOR);
         CLUSTER.createTopic("output77", 1, REPLICATION_FACTOR);
         CLUSTER.createTopic("output8", 1, REPLICATION_FACTOR);
+        CLUSTER.createTopic("output9", 1, REPLICATION_FACTOR);
 
 
 
@@ -849,7 +851,101 @@ public class SiddhiControllerIntegrationTest {
 
     }
 
+    @Test
+    public void SiddhiControllerShouldNotSendAnEmptyMessageStreamTest() throws InterruptedException {
 
+        String jsonData = "{\"attributeNotUsed\":2}";
+        String jsonData2 =  "{\"attributeName\":2}";
+
+        KeyValue<String, Map<String, Object>> kvStream1 = null;
+        KeyValue<String, Map<String, Object>> kvStream2 = null;
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            kvStream1 = new KeyValue<>("KEY_A", objectMapper.readValue(jsonData, Map.class));
+            kvStream2 = new KeyValue<>("KEY_A", objectMapper.readValue(jsonData2, Map.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        Properties producerConfig = new Properties();
+        producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
+        producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
+        producerConfig.put(ProducerConfig.RETRIES_CONFIG, 0);
+        producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, Serdes.String().serializer().getClass());
+        producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+
+        SiddhiController siddhiController = SiddhiController.getInstance();
+
+        SourceModel sourceModel = new SourceModel("stream9", "input9");
+        List<SourceModel> sourceModelList = new LinkedList<>();
+        sourceModelList.add(sourceModel);
+
+        SinkModel sinkModel = new SinkModel("streamoutput9", "output9");
+        List<SinkModel> sinkModelList = new LinkedList<>();
+        sinkModelList.add(sinkModel);
+
+        String id = "rule9";
+        String version = "v1";
+        String executionPlan = "from stream9 select * insert into streamoutput9";
+        Map<String, Object> options = new HashMap<>();
+        options.put("filterOutputNullDimension", true);
+
+        StreamMapModel streamMapModel = new StreamMapModel(sourceModelList, sinkModelList);
+
+        RuleModel ruleModelObject = new RuleModel(id, version, streamMapModel, executionPlan, options);
+
+
+        List<RuleModel> ruleModelList = new LinkedList<>();
+        ruleModelList.add(ruleModelObject);
+
+
+        List<StreamModel> streamsModel = Arrays.asList(
+                new StreamModel("stream9", Arrays.asList(
+                        new AttributeModel("attributeName", "integer"), new AttributeModel("attributeName2", "integer")
+                )));
+
+        ProcessingModel processingModel = new ProcessingModel(ruleModelList, streamsModel);
+
+        siddhiController.addProcessingDefinition(processingModel);
+        siddhiController.generateExecutionPlans();
+        siddhiController.addProcessingModel2KafkaController();
+
+        Properties consumerConfigA = new Properties();
+        consumerConfigA.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
+        consumerConfigA.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group-consumer-A");
+        consumerConfigA.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerConfigA.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        consumerConfigA.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("attributeName", 2);
+
+
+
+        KeyValue<String, Map<String, Object>> expectedDataKv = new KeyValue<>(null, expectedData);
+
+
+        try {
+            System.out.println("Producing KVs: " + kvStream1);
+            IntegrationTestUtils.produceKeyValuesSynchronously("input9", Collections.singletonList(kvStream1), producerConfig, MOCK_TIME);
+            IntegrationTestUtils.produceKeyValuesSynchronously("input9", Collections.singletonList(kvStream2), producerConfig, MOCK_TIME);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        List<KeyValue<String, Map>> receivedMessagesFromOutput1 = IntegrationTestUtils.waitUntilMinKeyValueRecordsReceived(consumerConfigA, "output9", 1);
+        System.out.println("Received after Siddhi: " + receivedMessagesFromOutput1);
+        assertEquals(Arrays.asList(expectedDataKv), receivedMessagesFromOutput1);
+
+    }
 
     @AfterClass
     public static void stop() {
