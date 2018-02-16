a04595d92876eb86bfd52648c83573d271093273
Andrés Gómez <andresgomezfrr@gmail.com>
2018-02-16 10:50:09 +0100

    WIZZIE-942 Fix Kafka JVM metrics at zz-cep (#39)

--- service/src/main/java/io/wizzie/ks/cep/builder/Builder.java
+++ service/src/main/java/io/wizzie/ks/cep/builder/Builder.java
@@ -7,6 +7,7 @@ import io.wizzie.ks.cep.controllers.SiddhiController;
 import io.wizzie.ks.cep.model.SiddhiAppBuilder;
 import io.wizzie.metrics.MetricsManager;
 import io.wizzie.ks.cep.model.ProcessingModel;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.streams.KafkaStreams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,10 +19,10 @@ import java.util.Properties;
 
 import static io.wizzie.ks.cep.builder.config.ConfigProperties.*;
 import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.admin.AdminClientConfig.CLIENT_ID_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
 import static org.apache.kafka.clients.producer.ProducerConfig.*;
 import static org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG;
-import static org.apache.kafka.streams.StreamsConfig.NUM_STREAM_THREADS_CONFIG;
 
 
 public class Builder implements Listener {
@@ -53,7 +54,6 @@ public class Builder implements Listener {
         this.siddhiAppBuilder = new SiddhiAppBuilder();
         this.config = config;
         metricsManager = new MetricsManager(config.getMapConf());
-        registerKafkaMetrics(config, metricsManager);
         metricsManager.start();
 
         if(siddhiController == null) {
@@ -65,6 +65,7 @@ public class Builder implements Listener {
         consumerProperties.put(BOOTSTRAP_SERVERS_CONFIG, config.get(KAFKA_CLUSTER));
 
         consumerProperties.put(GROUP_ID_CONFIG,  String.format("%s_%s", config.get(APPLICATION_ID), "zz-cep"));
+        consumerProperties.put(CLIENT_ID_CONFIG, String.format("%s_%s", config.get(APPLICATION_ID), "zz-cep"));
         consumerProperties.put(ENABLE_AUTO_COMMIT_CONFIG, "true");
         consumerProperties.put(AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         consumerProperties.put(KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
@@ -74,6 +75,7 @@ public class Builder implements Listener {
         Properties producerProperties = new Properties();
         producerProperties.putAll(config.getMapConf());
         producerProperties.put(BOOTSTRAP_SERVERS_CONFIG, config.get(KAFKA_CLUSTER));
+        producerProperties.put(CLIENT_ID_CONFIG, String.format("%s_%s", config.get(APPLICATION_ID), "zz-cep"));
         producerProperties.put(KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         producerProperties.put(PARTITIONER_CLASS_CONFIG, "io.wizzie.ks.cep.connectors.kafka.KafkaPartitioner");
         producerProperties.put(VALUE_SERIALIZER_CLASS_CONFIG, config.getOrDefault(VALUE_SERIALIZER, "io.wizzie.ks.cep.serializers.JsonSerializer"));
@@ -106,60 +108,60 @@ public class Builder implements Listener {
                 siddhiController.addProcessingModel2KafkaController();
             }
         } catch (Exception e) {
-            log.error(e.getMessage());
+            log.error(e.getMessage(), e);
         }
 
+        registerKafkaMetrics(config, metricsManager);
         log.info("Started CEP with conf {}", config.getProperties());
     }
 
     private void registerKafkaMetrics(Config config, MetricsManager metricsManager){
-        Integer streamThreads = config.getOrDefault(NUM_STREAM_THREADS_CONFIG, 1);
         String appId = config.get(APPLICATION_ID_CONFIG);
 
-        for (int i = 1; i <= streamThreads; i++) {
+        log.info("Register kafka jvm metrics: ");
+
             try {
 
                 // PRODUCER
-                metricsManager.registerMetric("producer." + i + ".messages_send_per_sec",
+                log.info(" * {}", "producer.messages_send_per_sec");
+                metricsManager.registerMetric("producer.messages_send_per_sec",
                         new JmxAttributeGauge(new ObjectName("kafka.producer:type=producer-metrics,client-id="
-                                + appId + "-1-StreamThread-"
-                                + i + "-producer"), "record-send-rate"));
+                                + String.format("%s_%s", appId, "zz-cep")), "record-send-rate"));
 
-                metricsManager.registerMetric("producer." + i + ".output_bytes_per_sec",
+                log.info(" * {}", "producer.output_bytes_per_sec");
+                metricsManager.registerMetric("producer.output_bytes_per_sec",
                         new JmxAttributeGauge(new ObjectName("kafka.producer:type=producer-metrics,client-id="
-                                + appId + "-1-StreamThread-"
-                                + i + "-producer"), "outgoing-byte-rate"));
+                                + String.format("%s_%s", appId, "zz-cep")), "outgoing-byte-rate"));
 
-                metricsManager.registerMetric("producer." + i + ".incoming_bytes_per_sec",
+                log.info(" * {}", "producer.incoming_bytes_per_sec");
+                metricsManager.registerMetric("producer.incoming_bytes_per_sec",
                         new JmxAttributeGauge(new ObjectName("kafka.producer:type=producer-metrics,client-id="
-                                + appId + "-1-StreamThread-"
-                                + i + "-producer"), "incoming-byte-rate"));
+                                + String.format("%s_%s", appId, "zz-cep")), "incoming-byte-rate"));
 
                 // CONSUMER
-                metricsManager.registerMetric("consumer." + i + ".max_lag",
+                log.info(" * {}", "consumer.max_lag");
+                metricsManager.registerMetric("consumer.max_lag",
                         new JmxAttributeGauge(new ObjectName("kafka.consumer:type=consumer-fetch-manager-metrics,client-id="
-                                + appId + "-1-StreamThread-"
-                                + i + "-consumer"), "records-lag-max"));
+                                + String.format("%s_%s", appId, "zz-cep")), "records-lag-max"));
 
-                metricsManager.registerMetric("consumer." + i + ".output_bytes_per_sec",
+                log.info(" * {}", "consumer.output_bytes_per_sec");
+                metricsManager.registerMetric("consumer.output_bytes_per_sec",
                         new JmxAttributeGauge(new ObjectName("kafka.consumer:type=consumer-metrics,client-id="
-                                + appId + "-1-StreamThread-"
-                                + i + "-consumer"), "outgoing-byte-rate"));
+                                + String.format("%s_%s", appId, "zz-cep")), "outgoing-byte-rate"));
 
-                metricsManager.registerMetric("consumer." + i + ".incoming_bytes_per_sec",
+                log.info(" * {}", "consumer.incoming_bytes_per_sec");
+                metricsManager.registerMetric("consumer.incoming_bytes_per_sec",
                         new JmxAttributeGauge(new ObjectName("kafka.consumer:type=consumer-metrics,client-id="
-                                + appId + "-1-StreamThread-"
-                                + i + "-consumer"), "incoming-byte-rate"));
+                                + String.format("%s_%s", appId, "zz-cep")), "incoming-byte-rate"));
 
-                metricsManager.registerMetric("consumer." + i + ".records_per_sec",
+                log.info(" * {}", "consumer.records_per_sec");
+                metricsManager.registerMetric("consumer.records_per_sec",
                         new JmxAttributeGauge(new ObjectName("kafka.consumer:type=consumer-fetch-manager-metrics,client-id="
-                                + appId + "-1-StreamThread-"
-                                + i + "-consumer"), "records-consumed-rate"));
+                                + String.format("%s_%s", appId, "zz-cep")), "records-consumed-rate"));
 
             } catch (MalformedObjectNameException e) {
-                log.warn("Metric not found");
+                log.error("kafka jvm metrics not found", e);
             }
-        }
     }
 
     public void close() throws Exception {
