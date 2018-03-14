6f370cc079faa1c73aa31ba083f06268c680c524
Juan Chorro <jjchorrobe@gmail.com>
2018-03-14 11:22:11 +0200

    MVP-493 Develop new file config
    * Add main
    
    * Fix mainClass
    
    * Add 'sample_config.json' file
    
    * Add 'sample_stream_definition.json' file
    
    * Add 'sample_processing_rules.json' file
    
    * Delete 'multi.id' property
    
    * Add booststrap.kafka.topics property

--- service/src/main/java/io/wizzie/ks/cep/builder/config/ConfigProperties.java
+++ service/src/main/java/io/wizzie/ks/cep/builder/config/ConfigProperties.java
@@ -6,7 +6,6 @@ public class ConfigProperties {
     public static final String METRIC_LISTENERS = "metric.listeners";
     public static final String METRIC_INTERVAL = "metric.interval";
     public static final String METRIC_VERBOSE_MODE = "metric.verbose.mode";
-    public static final String MULTI_ID = "multi.id";
     public static final String MAX_KAFKA_OUTPUT_TOPICS = "max.kafka.output.topics";
 }
 
