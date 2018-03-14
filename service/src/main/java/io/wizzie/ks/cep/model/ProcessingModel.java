608b5d27be9b2388c998ca1c7c74bbee895e7e0c
Juan Jesus Chorro Bergillo <jjchorrobe@gmail.com>
2018-03-14 11:22:11 +0200

    Add getter and setter

--- service/src/main/java/io/wizzie/ks/cep/model/ProcessingModel.java
+++ service/src/main/java/io/wizzie/ks/cep/model/ProcessingModel.java
@@ -1,11 +1,25 @@
 package io.wizzie.ks.cep.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class ProcessingModel {
     List<RuleModel> rules;
 
-    public ProcessingModel(List<RuleModel> rules) {
+    @JsonCreator
+    public ProcessingModel(@JsonProperty("rules") List<RuleModel> rules) {
+        this.rules = rules;
+    }
+
+    @JsonProperty
+    public List<RuleModel> getRules() {
+        return rules;
+    }
+
+    @JsonProperty
+    public void setRules(List<RuleModel> rules) {
         this.rules = rules;
     }
 }
