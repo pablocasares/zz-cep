4ae27bd1e88bbab31b945e91f0d30e3807ce3175
Juan Jesus Chorro Bergillo <jjchorrobe@gmail.com>
2018-03-14 11:22:11 +0200

    Add getter and setter

--- service/src/main/java/io/wizzie/ks/cep/model/StreamModel.java
+++ service/src/main/java/io/wizzie/ks/cep/model/StreamModel.java
@@ -12,6 +12,28 @@ public class StreamModel {
     @JsonCreator
     public StreamModel(@JsonProperty("streamName") String streamName,
                        @JsonProperty("attributes") List<AttributeModel> attributes) {
-
+        this.streamName = streamName;
+        this.attributes = attributes;
     }
+
+    @JsonProperty
+    public String getStreamName() {
+        return streamName;
+    }
+
+    @JsonProperty
+    public void setStreamName(String streamName) {
+        this.streamName = streamName;
+    }
+
+    @JsonProperty
+    public List<AttributeModel> getAttributes() {
+        return attributes;
+    }
+
+    @JsonProperty
+    public void setAttributes(List<AttributeModel> attributes) {
+        this.attributes = attributes;
+    }
+
 }
