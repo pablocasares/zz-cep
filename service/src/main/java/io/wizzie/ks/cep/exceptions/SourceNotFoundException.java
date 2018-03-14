d991e25c67c2067f0af66027168b8ddda36c4edf
Juan Jesus Chorro Bergillo <jjchorrobe@gmail.com>
2018-03-14 11:22:11 +0200

    Add 'SourceNotFoundException' class

---
+++ service/src/main/java/io/wizzie/ks/cep/exceptions/SourceNotFoundException.java
@@ -0,0 +1,8 @@
+package io.wizzie.ks.cep.exceptions;
+
+public class SourceNotFoundException extends RuntimeException {
+
+    public SourceNotFoundException(String message) {
+        super(message);
+    }
+}
