90df395bbc8a2f10fb8c0f7ea10588ef369fb01e
Andrés Gómez <andresgomezfrr@gmail.com>
2018-06-20 11:16:59 +0200

    WIZZIE-2038 Change exec command at zz-cep

--- bin/cep-start.sh
+++ bin/cep-start.sh
@@ -14,4 +14,4 @@ do
     CLASSPATH=${CLASSPATH}:${file}
 done
 
-java ${JVM_OPTIONS} -cp ${CLASSPATH} io.wizzie.ks.cep.Cep $1
+exec java ${JVM_OPTIONS} -cp ${CLASSPATH} io.wizzie.ks.cep.Cep $1
--- docker/build/cep-start.sh
+++ docker/build/cep-start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 envsubst < /opt/zz-cep/config/config_env.json > /opt/zz-cep/config/config.json
 envsubst < /opt/zz-cep/config/log4j2_env.xml > /opt/zz-cep/config/log4j2.xml
-/opt/zz-cep/bin/cep-start.sh /opt/zz-cep/config/config.json
+
+exec /opt/zz-cep/bin/cep-start.sh /opt/zz-cep/config/config.json
